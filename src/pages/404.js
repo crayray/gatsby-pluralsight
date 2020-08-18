d259431c4355372ebb6149cee1c23dcfcc7f247b
rachfuerst <rach.fuerst@gmail.com>
2020-08-18 17:54:28 -0500

    Creating CSS plugin

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,11 @@
 import React from "react"
+import styles from "./404.module.css"
 
 export default function Home() {
-  return <div>Hello from 404!</div>
+  return (
+    <div>
+      <h1 className={styles.header}>Hello from 404!</h1>
+      <p className={styles.errorMessage}>The page you are looking for does not exist.</p>
+    </div>
+  )
 }
