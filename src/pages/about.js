b2b5daf40c5149c4f951a1874181e0cec8854fe1
rachfuerst <rach.fuerst@gmail.com>
2020-08-18 18:20:52 -0500

    Adding SCSS modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
+import styles from "./about.module.scss"
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.content}>
     <Link to="/">Home</Link> | <Link to="/about">About Me</Link> 
       <h1>Hello from About!</h1>
     </div>
